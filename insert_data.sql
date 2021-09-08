ffb282cfc12424c8a5a4c2b171e650e5b74c368a
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2021-09-08 18:02:36 -0400

    add data

---
+++ insert_data.sql
@@ -0,0 +1,18 @@
+CREATE TABLE participation (
+    name TEXT,
+    year INTEGER
+);
+
+INSERT INTO participation (name, year) VALUES 
+    ("John", 2003),
+    ("Lyla", 1994),
+    ("Faith", 1996),
+    ("John", 2002),
+    ("Carol", 2000),
+    ("Carol", 1999),
+    ("John", 2001),
+    ("Carol", 2002),
+    ("Lyla", 1996),
+    ("Lyla", 1997),
+    ("Carol", 2001),
+    ("John", 2009);
